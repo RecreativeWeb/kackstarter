58696069b231bedfc46f65fc37be6e496e10821e
RecreativeWeb <apoeydo@gmail.com>
2017-11-11 18:30:36 +0100

    generation des getters et setters pour notre classe Project grace à Doctrine (generate entities)

--- src/AppBundle/Entity/Project.php
+++ src/AppBundle/Entity/Project.php
@@ -31,4 +31,86 @@ class Project
 	 * @ORM\Column(type="decimal", precision=15, scale=2)
 	 */
 	private $target_amount;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Project
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Project
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set targetAmount
+     *
+     * @param string $targetAmount
+     *
+     * @return Project
+     */
+    public function setTargetAmount($targetAmount)
+    {
+        $this->target_amount = $targetAmount;
+
+        return $this;
+    }
+
+    /**
+     * Get targetAmount
+     *
+     * @return string
+     */
+    public function getTargetAmount()
+    {
+        return $this->target_amount;
+    }
 }
