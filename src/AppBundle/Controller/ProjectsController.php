3f5c11a5ff242863eceb8963a39312bfda360d8b
RecreativeWeb <apoeydo@gmail.com>
2017-11-11 19:25:53 +0100

    Utilisation de l'entity manager dans notre controller, pour pouvoir persister et flusher des données en BDD

--- src/AppBundle/Controller/ProjectsController.php
+++ src/AppBundle/Controller/ProjectsController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Project;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -12,10 +14,22 @@ class ProjectsController extends Controller
 	 */
 	public function indexAction()
 	{
-		$projects = ['Cinema ambulant','Theatre de rue','Origami d\'été','Papillote géante','Immeuble floral'];
+		$project = new Project;
+		$project->setName("Immeuble floral");
+		$project->setDescription("Quand la ville arbore son manteau automnal");
+		$project->setTargetAmount(0);
 
-		return $this->render('projects/index.html.twig',[
-			"projets" => $projects
-		]);
+		$em = $this->getDoctrine()->getManager();
+
+		$em->persist($project);
+		$em->flush();
+
+		return new Response('<h1>Project created!</h1>');
+
+		//$projects = ['Cinema ambulant','Theatre de rue','Origami d\'été','Papillote géante','Immeuble floral'];
+
+		//return $this->render('projects/index.html.twig',[
+		//	"projets" => $projects
+		//]);
 	}
 }
